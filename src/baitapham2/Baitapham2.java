34002cfe7fc52e8c95be256d2fad0f385c5615be
DELL <DELL@DESKTOP-OGRVV23>
2021-10-13 14:26:42 +0700

    baitapham2

---
+++ src/baitapham2/Baitapham2.java
@@ -0,0 +1,18 @@
+
+package baitapham2;
+
+import java.util.Scanner;
+public class Baitapham2 {
+    public static void trave() {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.print("Nhập chuỗi kí tự: ");
+    	String string = scanner.next();
+    	System.out.println(string);
+    	
+    }
+	public static void main(String[] args) {
+		trave();
+
+	}
+
+}
